94aa95db154b04a6451d1e3eac9dcae2a1df0dc1
Lokesh Naik <54623523+lokeshnaik-01@users.noreply.github.com>
2024-05-29 13:44:11 +0000

    output dynamic value

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
 function Header() {
+  const desc = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
+      <p>We use curly braces to output dynamic values</p>
+
       <p>
-        Fundamental React concepts you will need for almost any app you are
-        going to build!
+        {desc} React concepts you will need for almost any app you are going to
+        build!
       </p>
       <p>We have created a new component header</p>
     </header>
