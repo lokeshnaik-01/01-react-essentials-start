43768ced8350c3634bd5c09b594866b8786b03d0
Lokesh Naik <54623523+lokeshnaik-01@users.noreply.github.com>
2024-05-29 13:36:57 +0000

    component readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# React 
+- root component will have many components which will create a hierarchy
+- custom components should start with uppercase letter as all html components are small case like div, image, etc
--- src/index.jsx
+++ src/index.jsx
@@ -5,3 +5,7 @@ import "./index.css";
 
 const entryPoint = document.getElementById("root");
 ReactDOM.createRoot(entryPoint).render(<App />);
+// the root we are getting it from index.html
+
+// root component will have many components which will create a hierarchy
+// custom components should start with uppercase letter as all html components are small case like div, image, etc
