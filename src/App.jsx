733a47f7b5f8714518969b53ab1677b4e985f643
Lokesh Naik <54623523+lokeshnaik-01@users.noreply.github.com>
2024-05-29 13:55:46 +0000

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-
+import componentImg from "./assets/components.png";
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
 function genRandomInt(max) {
@@ -23,6 +23,16 @@ function Header() {
   );
 }
 
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.image} alt="..." />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+      <p>Props is set by react we just send all the values</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
@@ -40,6 +50,16 @@ function App() {
       <p>We can directly call the function as a HTML tag</p>
       <main>
         <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="Description"
+              image={componentImg}
+            />
+          </ul>
+        </section>
       </main>
     </div>
   );
