f70c8fc56575ae053fa005acce98a2139cc52378
Lokesh Naik <54623523+lokeshnaik-01@users.noreply.github.com>
2024-06-16 12:09:46 +0000

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,4 @@
-import reactImg from "./assets/react-core-concepts.png";
-import componentImg from "./assets/components.png";
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-import { useState, Fragment } from "react";
 import Header from "./components/Header";
-import CoreConcept from "./components/CoreConcept";
-import CoreConceptDestruct from "./components/CoreConceptDestruct";
-import TabButton from "./components/TabButton";
-import { CORE_CONCEPTS } from "./data";
-import { EXAMPLES } from "./data";
 import CoreConcepts from "./components/CoreConcepts";
 import SampleText from "./components/SampleText";
 import Examples from "./components/Examples";
